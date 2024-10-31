4e9cb9c4cef372bdce1cd0c1182a007185e60fc5
RicardoBachmann <ricardo.bachmann@icloud.com>
2024-10-31 08:42:37 +0100

    Create Card component and render data

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 
-export default function Card() {
-  return <></>;
+export default function Card(props) {
+  return (
+    <>
+      <div className="card--container">
+        <h1>Countries:</h1>
+        <ul className="card--countries">
+          {props.countries.map((country, index) => (
+            <li key={index}>{country}</li>
+          ))}
+        </ul>
+        <p className="card--description">{props.description}</p>
+        <ul>
+          {props.issues.map((issue, index) => (
+            <li key={index}>{issue}</li>
+          ))}
+        </ul>
+        <div className="map--container"></div>
+      </div>
+    </>
+  );
 }
