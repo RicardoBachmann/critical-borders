253527f25f2f17c163583bf45667bb7ffff97c55
RicardoBachmann <ricardo.bachmann@icloud.com>
2024-10-31 11:23:31 +0100

    Add map component..currently not working

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,36 @@
-import "./App.css";
+import { useRef, useEffect } from "react";
+import mapboxgl from "mapbox-gl";
+
 import Card from "./Components/Card.jsx";
 import data from "./data.js";
-
-console.log(data);
+import "./App.css";
 
 function App() {
+  // first ref will presist the map instance
+  const mapRef = useRef();
+  // second ref exposes the map container's element
+  const mapContainerRef = useRef();
+
+  useEffect(() => {
+    mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN;
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      style: "mapbox://styles/mapbox/streets-v11",
+      center: [13.405, 52.52],
+      zoom: 9,
+    });
+
+    return () => {
+      mapRef.current.remove();
+    };
+  }, []);
+
   const displayData = data.map((item) => {
     return <Card key={item.id} {...item} />;
   });
   return (
     <>
+      <div id="map-container" ref={mapContainerRef}></div>
       <section>{displayData}</section>
     </>
   );
